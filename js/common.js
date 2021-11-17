611c24d31858c414da6612827426c2178db57731
jokeejoke <69756862+jokeejoke@users.noreply.github.com>
2021-11-17 20:53:29 +0600

    commit

--- js/common.js
+++ js/common.js
@@ -1,5 +1,9 @@
-document.addEventListener('DOMContentLoaded', function() {
-
-	// $('body').hide()
-
-})
+document.addEventListener("DOMContentLoaded", function () {
+  $(".offcanvas-nav").on("click", function () {
+    $("body").addClass("sidr-open");
+  });
+  $(".sidr-class-sidr-button-close").on("click", function (e) {
+    $("body").removeClass("sidr-open");
+    e.preventDefault();
+  });
+});
