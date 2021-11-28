9ec710c2e135e9eb87374622190a4fb730b83acb
Максат Куанышов <maqsat@MacBook-Air-Maksat.local>
2021-11-28 16:11:55 +0600

    add register and sign in form

--- js/common.js
+++ js/common.js
@@ -6,4 +6,21 @@ document.addEventListener("DOMContentLoaded", function () {
     $("body").removeClass("sidr-open");
     e.preventDefault();
   });
+
+  var dateMask = IMask(
+    document.getElementById('birth_day'),
+    {
+      mask: Date,
+      min: new Date(1950, 0, 1),
+      max: new Date(2020, 0, 1),
+      lazy: false
+    });
+
+  var phoneMask = IMask(
+    document.getElementById('phone'), {
+      mask: '+{7} (000)-000-00-00'
+    });
+
+    $('.theme-select').select2();
+
 });
