28cac243dc368ab59cad16118b5c2064525af1a1
Максат Куанышов <maqsat@MacBook-Air-Maksat.local>
2021-11-30 17:26:33 +0600

    commit

--- js/common.js
+++ js/common.js
@@ -7,6 +7,26 @@ document.addEventListener("DOMContentLoaded", function () {
     e.preventDefault();
   });
 
+  $( ".datepicker" ).datepicker();
+  $.datepicker.regional['ru'] = {clearText: 'Закрыть', clearStatus: '',
+    closeText: 'Fermer', closeStatus: 'Fermer sans modifier',
+    prevText: 'Пред', prevStatus: 'Voir le mois précédent',
+    nextText: 'След;', nextStatus: 'Voir le mois suivant',
+    currentText: 'Текущий', currentStatus: 'Voir le mois courant',
+    monthNames: ['Январь','Февраль','Март','Апрель','Май','Июнь',
+    'Июль','Август','Сентрябрь','Октябрь','Ноябрь','Декабрь'],
+    monthNamesShort: ['Янв','Фев','Мар','Апр','Май','Инь',
+    'Июь','Авг','Сен','Окт','Ноя','Дек'],
+    monthStatus: 'Voir un autre mois', yearStatus: 'Voir un autre année',
+    weekHeader: 'Sm', weekStatus: '',
+    dayNames: ['Воскресенье','Понедельник','Вторник','Среда','Четверг','Пятница','Суббота'],
+    dayNamesShort: ['Воскр','Пн','Вт','Ср','Чет','Пят','Суб'],
+    dayNamesMin: ['Вк','Пн','Вт','Ср','Чт','Пт','Сб'],
+    dayStatus: 'Utiliser DD comme premier jour de la semaine', dateStatus: 'Choisir le DD, MM d',
+    dateFormat: 'dd.mm.yy', firstDay: 1, 
+    initStatus: 'Choisir la date', isRTL: false};
+ $.datepicker.setDefaults($.datepicker.regional['ru']);
+
   const dateMask = document.getElementById('birth_day');
   if (dateMask) {
     IMask(
